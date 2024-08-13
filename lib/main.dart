c05011a517f9df393193508302848049495c483a
SEC-MASUM <masum.jess42@gmail.com>
2024-08-14 00:47:14 +0600

    Add Aditional text, an image from internet, an interactive elevated button with custom styling.

--- lib/main.dart
+++ lib/main.dart
@@ -11,34 +11,62 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: "Home"),
+      // theme: ThemeData(
+      //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      //   useMaterial3: true,
+      // ),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
+  MySnackBar(message, context) {
+    return ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        //backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Greating App"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Hello, World!",
+              style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            Text("Welcome to Flutter!"),
+            SizedBox(height: 20),
+            Image.network(
+              "https://iconape.com/wp-content/files/yb/61798/png/flutter-logo.png",
+              height: 100,
+              width: 100,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                MySnackBar("Button Pressed!", context);
+              },
+              child: Text("Press Me"),
+              style: ButtonStyle(
+                backgroundColor: WidgetStateProperty.all<Color>(Colors.green),
+                foregroundColor: WidgetStateProperty.all<Color>(Colors.white),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
